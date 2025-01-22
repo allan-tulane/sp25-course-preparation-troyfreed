6d02ec923b27d731c5f545cd3b6be246927f28cb
troyfreed <96844527+troyfreed@users.noreply.github.com>
2025-01-22 22:07:40 +0000

    FirstUpdate

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ CMPS 2200  Preparation
 def myfunction(input):
 	""" done. """
 	return input
+print(myfunction('Hello World'))
 
 
 
--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ def test_one():
 	""" done. """
 	assert myfunction('Hello') == 'Hello'
 	assert myfunction(1) == 1
-	assert myfunction(6) == -1
+	assert myfunction(6) == 6
 
 def test_two():
 	assert myfunction(3+4) == 7
